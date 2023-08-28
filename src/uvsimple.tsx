0e1a5d319a6bd3b2d7cbfae9957653aabe04c638
jihed krouna <jihedkrouna@gmail.com>
2023-08-28 13:30:40 -0400

    final

--- src/uvsimple.tsx
+++ src/uvsimple.tsx
@@ -8,6 +8,7 @@ const Uvsimple = () => {
 	const uvRef = useRef<HTMLDivElement | null>(null);
 	const manifestUrls = useMemo(
 		() => [
+			"https://www.e-rara.ch/i3f/v20/6069569/manifest",
 			"https://edsilv.github.io/test-manifests/download-service-enabled.json",
 			"https://wellcomelibrary.org/iiif/b18035723/manifest",
 			"https://dms-data.stanford.edu/data/manifests/RomanCoins/bb853kn3021/manifest.json",
@@ -17,54 +18,59 @@ const Uvsimple = () => {
 	);
 
 	const [currentManifestIndex, setCurrentManifestIndex] = useState<number>(0);
-	const [manifestTitles, setManifestTitles] = useState<string[]>([]);
+	// const [manifestTitles, setManifestTitles] = useState<string[]>([]);
 
 	useEffect(() => {
 		const initViewer = async () => {
+			const startime = performance.now();
 			const data = {
 				manifest: manifestUrls[currentManifestIndex],
 				embedded: true,
 			};
-			init("uv", data);
+			const uv = init("uv", data);
+			uv.on("openseadragonExtension.animationFinish", () => {
+				const endtime = performance.now();
+				console.log("load time", endtime - startime);
+			});
 		};
 
 		initViewer();
 	}, [currentManifestIndex, manifestUrls]);
 
-	useEffect(() => {
-		const fetchManifestTitles = async () => {
-			try {
-				const titles = await Promise.all(
-					manifestUrls.map(async (url) => {
-						const response = await fetch(url);
-						const jsonObject = await response.json();
-						return jsonObject.label; // Assuming the title is under the "label" property; adjust accordingly if needed.
-					})
-				);
-				setManifestTitles(titles);
-			} catch (error) {
-				console.error("Error fetching manifest titles:", error);
-			}
-		};
+	// useEffect(() => {
+	// 	const fetchManifestTitles = async () => {
+	// 		try {
+	// 			const titles = await Promise.all(
+	// 				manifestUrls.map(async (url) => {
+	// 					const response = await fetch(url);
+	// 					const jsonObject = await response.json();
+	// 					return jsonObject.label; // Assuming the title is under the "label" property; adjust accordingly if needed.
+	// 				})
+	// 			);
+	// 			setManifestTitles(titles);
+	// 		} catch (error) {
+	// 			console.error("Error fetching manifest titles:", error);
+	// 		}
+	// 	};
 
-		fetchManifestTitles();
-	}, [manifestUrls]);
+	// 	fetchManifestTitles();
+	// }, [manifestUrls]);
 
-	const handleManifestChange = (index: number) => {
-		setCurrentManifestIndex(index);
-	};
+	// const handleManifestChange = (index: number) => {
+	// 	setCurrentManifestIndex(index);
+	// };
 
-	const handleNextManifest = () => {
-		setCurrentManifestIndex((prevIndex) =>
-			prevIndex === manifestUrls.length - 1 ? 0 : prevIndex + 1
-		);
-	};
+	// const handleNextManifest = () => {
+	// 	setCurrentManifestIndex((prevIndex) =>
+	// 		prevIndex === manifestUrls.length - 1 ? 0 : prevIndex + 1
+	// 	);
+	// };
 
-	const handlePreviousManifest = () => {
-		setCurrentManifestIndex((prevIndex) =>
-			prevIndex === 0 ? manifestUrls.length - 1 : prevIndex - 1
-		);
-	};
+	// const handlePreviousManifest = () => {
+	// 	setCurrentManifestIndex((prevIndex) =>
+	// 		prevIndex === 0 ? manifestUrls.length - 1 : prevIndex - 1
+	// 	);
+	// };
 
 	return (
 		<>
@@ -73,7 +79,8 @@ const Uvsimple = () => {
 					className="uv"
 					id="uv"
 					ref={uvRef}></div>
-				<div className="list-container">
+
+				{/* <div className="list-container">
 					<h3>List of manifest available</h3>
 					<ul className="list-manifest">
 						{manifestTitles.map((title, index) => (
@@ -100,10 +107,25 @@ const Uvsimple = () => {
 					className="button-manifest"
 					onClick={handleNextManifest}>
 					<span>Next</span>
-				</button>
+				</button>*/}
 			</div>
 		</>
 	);
 };
 
 export default Uvsimple;
+
+// with npm run start:
+// 10k first
+
+// 10k second
+
+// 11k 3rd
+
+// with serve :
+
+// 6.1k first time
+
+// 3.4k second time
+
+// 6k 3rd time
